9d3ea4841dd463343138775130063b7cbc054cd2
Gina <gina@ginazampino.com>
2018-08-23 21:54:00 -0400

    Improved media queries.

--- front_end/components/app-portfolio.vue
+++ front_end/components/app-portfolio.vue
@@ -15,30 +15,35 @@
                 </div>
             </div>
             <div class="portfolio-contact">
-                <ul>
+                <ul class="portfolio-contact-list">
                     <li>
                         <a href="https://goo.gl/maps/J4d62KNtVnH2" target="_blank">
-                            <i class="fas fa-map-marker-alt"></i><span>Toronto, CA</span>
+                            <i class="fas fa-map-marker-alt"></i>
+                            <span class="i-label">Toronto, CA</span>
                         </a>
                     </li>
                     <li>
                         <a href="https://www.linkedin.com/in/ginazampino/" target="_blank">
-                            <i class="fab fa-linkedin-in"></i><span>/in/ginazampino</span>
+                            <i class="fab fa-linkedin-in"></i>
+                            <span class="i-label">/in/ginazampino</span>
                         </a>
                     </li>
                     <li>
                         <a href="https://github.com/ginazampino" target="_blank">
-                            <i class="fab fa-github"></i><span>ginazampino</span>
+                            <i class="fab fa-github"></i>
+                            <span class="i-label">ginazampino</span>
                         </a>
                     </li>
                     <li>
                         <a href="tel:343-333-1721">
-                            <i class="fas fa-phone"></i><span>+1 (343) 333-1721</span>
+                            <i class="fas fa-phone"></i>
+                            <span class="i-label">+1 (343) 333-1721</span>
                         </a>
                     </li>
                     <li>
                         <a href="mailto:gina@ginazampino.com?subject=Website%20Inquiry">
-                            <i class="fas fa-envelope"></i><span>gina@ginazampino.com</span>
+                            <i class="fas fa-envelope"></i>
+                            <span class="i-label">gina@ginazampino.com</span>
                         </a>
                     </li>
                 </ul>
--- front_end/components/profile-summary.vue
+++ front_end/components/profile-summary.vue
@@ -5,7 +5,7 @@
         <p>
             Hi, there! I'm Gina Zampino, and I'm a junior front-end web developer based in Toronto, Canada.
             I'm currently looking for work,
-            so feel free to <a class="profile-a" href="#">get in touch</a> with contracts or full-time job opportunities.
+            so feel free to <a class="profile-a" href="mailto:gina@ginazampino.com?subject=Website%20Inquiry">get in touch</a> with contracts or full-time job opportunities.
         </p>
     </div>
 </template>
