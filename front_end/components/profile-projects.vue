af21839cb8ab68b44f3cadf73e4f5d2bfaea3a06
Gina <gina@ginazampino.com>
2018-08-24 00:41:27 -0400

    Designed project elements.

--- front_end/components/profile-projects.vue
+++ front_end/components/profile-projects.vue
@@ -1,8 +1,71 @@
 <template>
     <div class="profile-section">
         <h2>Projects</h2>
-        <p>
-            WIP
-        </p>
+        <ul>
+            <li class="project">
+                <div class="project-image">
+                    <ul class="project-tech">
+                        <li>
+                            <i class="devicon-html5-plain" title="HTML5"></i>
+                        </li>
+                        <li>
+                            <i class="devicon-css3-plain" title="CSS3"></i>
+                        </li>
+                        <li>
+                            <i class="devicon-sass-original" title="SCSS"></i>
+                        </li>
+                        <li>
+                            <i class="devicon-javascript-plain" title="JavaScript"></i>
+                        </li>
+                        <li>
+                            <i class="devicon-vuejs-plain" title="Vue.js"></i>
+                        </li>
+                        <li>
+                            <i class="devicon-webpack-plain" title="Webpack"></i>
+                        </li>
+                        <li>
+                            <i class="devicon-nodejs-plain" title="Node.js"></i>
+                        </li>
+                        <li>
+                            <i class="devicon-express-original" title="Express"></i>
+                        </li>
+                        <li>
+                            <i class="devicon-amazonwebservices-original" title="AWS"></i>
+                        </li>
+                        <li>
+                            <i class="devicon-visualstudio-plain" title="VS Code"></i>
+                        </li>
+                        <li>
+                            <i class="devicon-git-plain" title="Git"></i>
+                        </li>
+                        <li>
+                            <i class="devicon-github-plain" title="GitHub"></i>
+                        </li>
+                    </ul>
+                </div>
+                <div class="project-description">
+                    <div class="project-name">Developer Portfolio</div>
+                    <ul class="project-details">
+                        <li>
+                            <span class="project-details-label">
+                                <a class="profile-a" href="#" target="_blank">
+                                    Live
+                                </a>
+                            </span>
+                        </li>   
+                        <li>
+                            <span class="project-details-label">
+                                <a class="profile-a" href="#" target="_blank">
+                                    Source
+                                </a>
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+                <p>
+                    A responsive, single-page developer portfolio site built to demonstrate a fundamental understanding of basic web design and development skills.
+                </p>
+            </li>
+        </ul>
     </div>
 </template>
