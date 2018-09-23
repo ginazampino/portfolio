15e6c86d997a8188e91a860ab1211ed9060edbef
Gina Zampino <zampino.gina@gmail.com>
2018-09-23 12:00:07 -0400

    Updated production build process.

--- front_end/webpack.config.js
+++ front_end/webpack.config.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { VueLoaderPlugin } = require('vue-loader');
+
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 const isDevMode = process.env.NODE_ENV === 'development';
 
@@ -15,7 +19,7 @@ module.exports = {
     },
 
     output: {
-        filename: '[name].js',
+        filename: isDevMode ? '[name].js' : '[name].[hash].js',
         path: path.resolve(__dirname, '../wwwroot')
     },
 
@@ -31,7 +35,7 @@ module.exports = {
             {
                 test: /\.(css|scss)$/,
                 use: [
-                    { loader: 'style-loader' },
+                    { loader: isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader },
                     { loader: 'css-loader' },
                     { loader: 'sass-loader' }
                 ]
@@ -50,12 +54,34 @@ module.exports = {
         ]
     },
 
+    optimization: {
+        minimizer: [      
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true // set to true if you want JS source maps
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+
     plugins: [
+        // Build an HTML file which includes all the CSS & JS chunks:
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: path.join(__dirname, 'index.html'),
             chunks: ['main']
         }),
+
+        // Extract the CSS chunks into a minified, bundled CSS file:
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: isDevMode ? '[name].css' : '[name].[hash].css',
+            chunkFilename: isDevMode ? '[id].css' : '[id].[hash].css',
+        }),
+
+        // Support vue-loader:
         new VueLoaderPlugin()
     ],
 
